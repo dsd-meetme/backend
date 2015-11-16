65afb1d48162b1665a0acb5b255dac4e12ba057f
Claudio Cardinale <cardinale.claudio@gmail.com>
2015-11-16 20:12:33 +0100

    Login inserted with tests

--- tests/AuthTest.php
+++ tests/AuthTest.php
@@ -1,7 +1,6 @@
 <?php
 
-use Illuminate\Foundation\Testing\WithoutMiddleware;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
+
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class AuthTest extends TestCase
@@ -10,24 +9,38 @@ class AuthTest extends TestCase
 
     public function testNewUser()
     {
-        $response = $this->post('/auth/register', ['name'=>'test', 'email'=>'test@test.com', 'password_confirmation'=>'testtest', 'password'=>'testtest'], ['Accept'=>'application/json']);
+        $response = $this->json('POST','/auth/register', ['name'=>'test', 'email'=>'test@test.com', 'password_confirmation'=>'testtest', 'password'=>'testtest']);
         $response->seeStatusCode(302);
         $this->seeInDatabase("users", ['name'=>'test', 'email'=>'test@test.com']);
 
         //already logged (redirect to main page)
-        $response = $this->post('/auth/register', [], ['Accept'=>'application/json']);
+        $response = $this->json('POST','/auth/register', []);
         $response->seeStatusCode(302);
     }
 
     public function testErrorNewUser()
     {
-        $response = $this->post('/auth/register', ['name'=>'test', 'email'=>'test@test.com', 'password_confirmation'=>'atesttest', 'password'=>'testtest'], ['Accept'=>'application/json']);
+        $response = $this->json('POST','/auth/register', ['name'=>'test', 'email'=>'test@test.com', 'password_confirmation'=>'atesttest', 'password'=>'testtest']);
         $response->seeStatusCode(422);
         $this->dontSeeInDatabase("users", ['name'=>'test', 'email'=>'test@test.com']);
 
         //not already logged (no redirect to main page)
-        $response = $this->post('/auth/register', [], ['Accept'=>'application/json']);
+        $response = $this->json('POST','/auth/register', []);
         $response->seeStatusCode(422);
 
+    }
+
+    public function testLogin()
+    {
+        $response = $this->post('/auth/login', ['email'=>'testInit@test.com', 'password'=>'test'], ['Accept'=>'application/json']);
+        $response->seeStatusCode(302);
+        $this->assertEquals($this->baseUrl, $response->response->headers->get('location'));
+    }
+
+    public function testErrorLogin()
+    {
+        $response = $this->post('/auth/login', ['email'=>'testInit@test.com', 'password'=>'atest'], ['Accept'=>'application/json']);
+        $response->seeStatusCode(302);
+        $this->assertEquals($this->baseUrl.'/auth/login', $response->response->headers->get('location'));
     }
 }
