611bb7eca86295adeea3fcb87422066401d0fc21
Claudio Cardinale <cardinale.claudio@gmail.com>
2015-11-17 23:40:42 +0100

    Headers fixed

--- app/Http/Middleware/HttpHeaders.php
+++ app/Http/Middleware/HttpHeaders.php
@@ -19,8 +19,11 @@ class HttpHeaders
 
         $response->header('Access-Control-Allow-Origin', 'http://admin.plunner.com');
         $response->header('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT');
-        //$response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Authorization, X-Request-With');
-        //$response->header('Access-Control-Allow-Credentials', 'true');
+        $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Authorization, X-Request-With, X-XSFR-TOKEN');
+        $response->header('Access-Control-Allow-Credentials', 'true');
+        $response->header('Access-Control-Max-Age', '1000');
+        $response->header('Cache-Control', 'no-cache, must-revalidate');
+        $response->header('Expires', 'Mon, 26 Jul 1997 05:00:00 GMT');
 
         return $response;
     }
