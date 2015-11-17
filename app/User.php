326f4aec08cffc34adbd33b76342269130514e3c
Emil-Sila <emil.siladi@gmail.com>
2015-11-17 15:37:33 +0100

    Added Employee table and model with relationship to User

--- app/User.php
+++ app/User.php
@@ -59,4 +59,9 @@ class User extends Model implements AuthenticatableContract,
      * @var array
      */
     protected $hidden = ['password', 'remember_token'];
+
+    public function employees()
+    {
+        return $this->hasMany('App\Employee');
+    }
 }
