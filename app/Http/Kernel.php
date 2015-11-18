f4a409b2700fa77136ce32dc7d371892448c2cc1
Claudio Cardinale <cardinale.claudio@gmail.com>
2015-11-18 19:24:41 +0100

    csfr check removed

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -17,7 +17,7 @@ class Kernel extends HttpKernel
         \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
         \Illuminate\Session\Middleware\StartSession::class,
         \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-        \plunner\Http\Middleware\VerifyCsrfToken::class,
+        //\plunner\Http\Middleware\VerifyCsrfToken::class,
         \plunner\Http\Middleware\HttpHeaders::class,
     ];
 
