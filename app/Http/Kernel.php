0af7653ba2fd6e7d429f0d03a3b2b9e998bec4f9
Claudio Cardinale <cardinale.claudio@gmail.com>
2015-11-21 17:41:07 +0100

    Tymon middleware reinserted

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -30,6 +30,6 @@ class Kernel extends HttpKernel
         'auth' => \plunner\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'guest' => \plunner\Http\Middleware\RedirectIfAuthenticated::class,
-        'jwt.authandrefresh' => 'plunner\Http\Middleware\GetUserAndRefresh',
+        'jwt.authandrefresh' => 'Tymon\JWTAuth\Middleware\GetUserAndRefresh',
     ];
 }
