32bfa4a2ecd20a2b5b3725509261f53bdf1a5817
Emil-Sila <emil.siladi@gmail.com>
2015-11-23 20:43:35 +0100

    Edit on meetings table

--- database/migrations/2015_11_23_192653_create_calendars_table.php
+++ database/migrations/2015_11_23_192653_create_calendars_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateMeetingsTable extends Migration
+class CreateCalendarsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,12 +12,9 @@ class CreateMeetingsTable extends Migration
      */
     public function up()
     {
-        Schema::create('meetings', function (Blueprint $table) {
+        Schema::create('calendars', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('title');
-            $table->string('description');
-            $table->dateTime('meeting_start');
-            $table->dateTime('meeting_end');
+            $table->string('name');
             $table->timestamps();
         });
     }
@@ -29,6 +26,6 @@ class CreateMeetingsTable extends Migration
      */
     public function down()
     {
-        Schema::drop('meetings');
+        Schema::drop('calendars');
     }
 }
