03468476068265761e8d38fd482eaa5d4c5a0bef
Claudio Cardinale <cardinale.claudio@gmail.com>
2015-11-27 16:02:49 +0100

    fixes in policy verify process

--- app/Planner.php
+++ app/Planner.php
@@ -18,11 +18,11 @@ namespace plunner;
  * @property string $remember_token
  * @property \Carbon\Carbon $created_at
  * @property \Carbon\Carbon $updated_at
- * @property-read \Illuminate\Database\Eloquent\Collection|Group[] $groupsManaged
+ * @property-read \Illuminate\Database\Eloquent\Collection|\plunner\Group[] $groupsManaged
  * @property-read \plunner\Company $company
  * @property-read \Illuminate\Database\Eloquent\Collection|\plunner\Group[] $groups
  * @property-read \Illuminate\Database\Eloquent\Collection|\plunner\Meeting[] $meetings
- * @property-read \Illuminate\Database\Eloquent\Collection|\App\Calendar[] $calendars
+ * @property-read \Illuminate\Database\Eloquent\Collection|\plunner\Calendar[] $calendars
  * @method static \Illuminate\Database\Query\Builder|\plunner\Planner whereId($value)
  * @method static \Illuminate\Database\Query\Builder|\plunner\Planner whereName($value)
  * @method static \Illuminate\Database\Query\Builder|\plunner\Planner whereEmail($value)
@@ -38,5 +38,40 @@ class Planner extends Employee
     public function groupsManaged()
     {
         return $this->HasMany(Group::class);
+    }
+
+    /*
+    * for a planer employee the policyCheckable methods say if the planer can modify or not that part
+    */
+
+    /**
+     * @param Group $group
+     * @return bool
+     */
+    public function verifyGroup(Group $group)
+    {
+        $group = $this->groupsManaged()->where('id', $group->id)->first();
+
+        return (is_object($group) && $group->exists);
+    }
+
+    /**
+     * @param Employee $employee
+     * @return bool
+     */
+    public function verifyEmployee(Employee $employee)
+    {
+        $group = $this->groupsManaged()->whereHas('employees',function ($query) use ($employee) {$query->where('employees.id', $employee->id);})->first();
+
+        return (is_object($group) && $group->exists);
+    }
+
+    /**
+     * @param Company $company
+     * @return bool
+     */
+    public function verifyCompany(Company $company)
+    {
+        return false;
     }
 }
