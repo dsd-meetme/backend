b00219d7f862ceaac713ff7e4c0488cf5a29b9f5
Claudio Cardinale <cardinale.claudio@gmail.com>
2015-11-29 22:16:54 +0100

    stress controller

---
+++ app/Http/Controllers/StressController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace plunner\Http\Controllers;
+
+use plunner\Http\Controllers\Controller;
+use \Illuminate\Http\Request;
+use plunner\Http\Requests;
+use plunner\Http\Requests\Companies\Employees\EmployeeRequest;
+
+/**
+ * Class EmployeesController
+ * @package plunner\Http\Controllers\Companies\Employees
+ * @author Claudio Cardinale <cardi@thecsea.it>
+ * @copyright 2015 Claudio Cardinale
+ * @version 1.0.0
+ */
+class StressController extends Controller
+{
+    /**
+     * ExampleController constructor.
+     */
+    public function __construct()
+    {
+        config(['auth.model' => \plunner\Company::class]);
+        config(['jwt.user' => \plunner\Company::class]);
+    }
+
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        return \plunner\Employee::with('company')->all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(EmployeeRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
