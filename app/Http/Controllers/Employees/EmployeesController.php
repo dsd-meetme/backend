c7f4da3c70fac57cdb9eb54c1c1bbf79f1abd500
Miha <mihovilvinkovic@gmail.com>
2015-12-09 16:31:37 +0100

    employee controller remove show

--- app/Http/Controllers/Employees/EmployeesController.php
+++ app/Http/Controllers/Employees/EmployeesController.php
@@ -33,21 +33,4 @@ class EmployeesController extends Controller
         $employee = \Auth::user();
         return $employee;
     }
-
-    //TODO this is not RESTFUL, but it can be ok
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        $employee = Employee::findOrFail($id);
-        $this->authorize($employee);
-        return $employee;
-    }
-
-    //TODO why this? when we need this?
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -78,7 +78,7 @@ Route::group(['namespace' => 'Employees', 'prefix' => 'employees'], function() {
         });
     });
 
-    Route::resource('employees', 'EmployeesController', ['only' => ['index', 'show']]);
+    Route::resource('employees', 'EmployeesController', ['only' => ['index']]);
     Route::resource('groups', 'GroupsController', ['only' => ['index', 'show']]);
 
     Route::resource('meetings', 'MeetingsController');
