fa7de04997c49d965a1beef585afe08d21366e77
Claudio Cardinale <cardinale.claudio@gmail.com>
2015-12-11 18:09:44 +0100

    just todos

--- app/Http/Requests/Employees/MeetingRequest.php
+++ app/Http/Requests/Employees/MeetingRequest.php
@@ -28,8 +28,8 @@ class MeetingRequest extends Request
             'description' => 'required|max:255',
             'start_time' => 'required|date_format:Y-m-d H:i:s',
             'end_time' => 'required|date_format:Y-m-d H:i:s|after:meeting_start', //TODO I think taht the correct way is datae_format, but we have to define this with the frontend guys and test it with the database, we have also to consider timezone
-            'repeat' => 'required|integer',
-            'repetition_end_time' => 'date_format:Y-m-d',
+            'repeat' => 'required|integer', //TODO we skip this functionality
+            'repetition_end_time' => 'date_format:Y-m-d', //TODO we skip this functionality
             'is_scheduled' => 'required|boolean',
             'group_id' => 'required|exists:groups,id',
             'employee_id' => 'required|exists:employees,id',
