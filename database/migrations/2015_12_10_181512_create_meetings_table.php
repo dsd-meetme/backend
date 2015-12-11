28c668f1f5bb60ca7c1a9425b05b0a2d2bf5244e
Claudio Cardinale <cardinale.claudio@gmail.com>
2015-12-11 22:34:31 +0100

    create_meeting checkouted form planner

---
+++ database/migrations/2015_12_10_181512_create_meetings_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMeetingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('meetings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('description');
+            $table->integer('planner_id')->unsigned();
+            $table->foreign('planner_id')->references('id')->on('employees')->onDelete('cascade')->onUpdate('cascade');
+            $table->integer('group_id')->unsigned();
+            $table->foreign('group_id')->references('id')->on('groups')->onDelete('cascade')->onUpdate('cascade');
+            $table->dateTime('start_time')->nullabe()->default(NULL);
+            $table->integer('duration'); //minutes
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('meetings');
+    }
+}
