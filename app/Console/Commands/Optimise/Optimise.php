f1ed66174db63ec307cdee298e7999d8e1205c81
Claudio Cardinale <cardinale.claudio@gmail.com>
2015-12-12 16:06:13 +0100

    draft optimise command final structure

--- app/Console/Commands/Optimise/Optimise.php
+++ app/Console/Commands/Optimise/Optimise.php
@@ -1,85 +1,57 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * User: claudio
+ * Date: 12/12/15
+ * Time: 15.41
+ */
 
 namespace plunner\Console\Commands\Optimise;
 
-use Illuminate\Console\Command;
-use Illuminate\Console\Scheduling\Schedule;
+
+use plunner\company;
 
 /**
  * Class Optimise
- * @package plunner\Console\Commands\Optimise
  * @author Claudio Cardinale <cardi@thecsea.it>
  * @copyright 2015 Claudio Cardinale
  * @version 1.0.0
+ * @package plunner\Console\Commands\Optimise
  */
-class Optimise extends Command
+class Optimise
 {
     /**
-     * The name and signature of the console command.
-     *
-     * @var string
+     * @var Company
      */
-    protected $signature = 'optimise:meetings {companyId?}';
+    private $company;
 
     /**
-     * The console command description.
-     *
-     * @var string
+     * Optimise constructor.
+     * @param Company $company
      */
-    protected $description = 'Optimise meetings';
-
-    /*
-    * @var Schedule laravel schedule object needed to perform command in background
-    */
-    private $schedule;
-
-    /**
-     * Create a new command instance.
-     * @param Schedule $schedule
-     *
-     */
-    public function __construct(Schedule $schedule)
+    public function __construct(Company $company)
     {
-        parent::__construct();
-        $this->schedule = $schedule;
+        $this->company = $company;
     }
 
     /**
-     * Execute the console command.
-     *
-     * @return mixed
+     * @return Company
      */
-    public function handle()
+    public function getCompany()
     {
-        //
-        //TODO check if glpk is installed
-        //TODO multithreads
-        //TODO log exceptions and fire
-        //TODO insert a timeout
-        $solver = new Solver($this->schedule, $this->laravel);
-        $solver->setTimeSlots(4);
-        $solver->setMaxTimeSlots(4);
-        $solver->setUsers([1=>1,2,3]);
-        $solver->setMeetings([1=>1,2]);
-        $solver->setMeetingsDuration([1=>1,3]);
-        $solver->setMeetingsAvailability([
-            1=>[1=>1,0,0,0],
-            [1=>1,1,1,0],
-        ]);
-        $solver->setUsersAvailability([
-            1=>[1=>1,1,0,0],
-            [1=>1,1,1,0],
-            [1=>1,1,1,0],
-        ]);
-        $solver->setUsersMeetings([
-            1=>[1=>1,0],
-            [1=>1,1],
-            [1=>1,1],
-        ]);
-        $solver->solve();
-        print_r($solver->getOutput());
-        print_r($solver->getXResults());
-        print_r($solver->getYResults());
-        //TODO try...catch with destruct
+        return $this->company;
+    }
+
+    /**
+     * @param Company $company
+     */
+    public function setCompany($company)
+    {
+        $this->company = $company;
+    }
+
+    public function optmise()
+    {
+        //TODO ...
     }
 }
---
+++ app/Console/Commands/Optimise/OptimiseCommand.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace plunner\Console\Commands\Optimise;
+
+use Illuminate\Console\Command;
+use Illuminate\Console\Scheduling\Schedule;
+use plunner\Company;
+
+/**
+ * Class OptimiseCommand
+ * @package plunner\Console\Commands\Optimise
+ * @author Claudio Cardinale <cardi@thecsea.it>
+ * @copyright 2015 Claudio Cardinale
+ * @version 1.0.0
+ */
+class OptimiseCommand extends Command
+{
+    const BACKGROUND_MOD_MEX = 'background mode';
+    const BACKGROUND_COMPLETED_MEX = 'All background tasks started';
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'optimise:meetings {companyId?}  {--background}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Optimise meetings';
+
+    /*
+    * @var Schedule laravel schedule object needed to perform command in background
+    */
+    private $schedule;
+
+    /**
+     * Create a new command instance.
+     * @param Schedule $schedule
+     *
+     */
+    public function __construct(Schedule $schedule)
+    {
+        parent::__construct();
+        $this->schedule = $schedule;
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        //
+        //TODO check if glpk is installed
+        //TODO multithreads
+        //TODO log exceptions and fire
+        //TODO insert a timeout
+
+        //TODO try...catch with destruct
+        $companyId = $this->argument('companyId');
+        if(is_numeric($companyId))
+            $this->makeForeground(Company::findOrFail($companyId));
+        else
+            $this->syncAll();
+    }
+
+    private function syncAll()
+    {
+        $calendars = Caldav::all();
+        if($this->option('background')) {
+            \Log::debug(self::BACKGROUND_MOD_MEX);
+            $this->info(self::BACKGROUND_MOD_MEX);
+            foreach ($calendars as $calendar)
+                $this->makeBackground($calendar);
+            \Log::debug(self::BACKGROUND_COMPLETED_MEX);
+            $this->info(self::BACKGROUND_COMPLETED_MEX);
+        }else
+            foreach($calendars as $calendar)
+                $this->makeForeground($calendar);
+    }
+
+    /**
+     * optimise company via exec command
+     * @param Company $company
+     */
+    private function makeBackground(Company $company)
+    {
+        $event = $this->schedule->command('optimise:meetings '.$company->id)->withoutOverlapping();
+        if($event->isDue($this->laravel))
+            $event->run($this->laravel);
+    }
+
+    /**
+     * optimise company foreground
+     * @param Company $company
+     */
+    private function makeForeground(Company $company)
+    {
+        $this->info('Optimisation company '. $company->id.' started');
+        (new Optimise($company))->optmise();
+        $this->info('Optimisation '. $company->id.' completed');
+    }
+}
