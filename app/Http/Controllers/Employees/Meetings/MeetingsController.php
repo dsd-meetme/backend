2f9631b686c85b83c1efae437573650acbdde618
Claudio Cardinale <cardinale.claudio@gmail.com>
2015-12-13 22:44:43 +0100

    draft meetings controller

---
+++ app/Http/Controllers/Employees/Meetings/MeetingsController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace plunner\Http\Controllers\Employees\Groups;
+
+use Illuminate\Http\Request;
+
+use plunner\Group;
+use plunner\Employee;
+use plunner\Http\Requests;
+use plunner\Http\Controllers\Controller;
+use plunner\Meeting;
+
+class MeetingsController extends Controller
+{
+    /**
+     * ExampleController constructor.
+     */
+    public function __construct()
+    {
+        config(['auth.model' => \plunner\Employee::class]);
+        config(['jwt.user' => \plunner\Employee::class]);
+        $this->middleware('jwt.authandrefresh:mode-en');
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $employee = \Auth::user();
+        return $employee->meetings;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $meeting = Meeting::findOrFail($id);
+        $this->authorize($meeting); //TODO implement authorize
+        return $meeting;
+    }
+}
