bfedde4dc803b3e12402018bef086dea2444bb09
Claudio Cardinale <cardinale.claudio@gmail.com>
2015-12-13 21:41:26 +0100

    update git_receiver

--- git_receiver.php
+++ git_receiver.php
@@ -1,5 +1,5 @@
 <?php
 system("./git.sh -i .git_utility/key.key git fetch");
-system("./git.sh -i .git_utility/key.key git pull origin master");
+system("./git.sh -i .git_utility/key.key git pull origin dev-unstable");
 system('composer update');
 ?>
