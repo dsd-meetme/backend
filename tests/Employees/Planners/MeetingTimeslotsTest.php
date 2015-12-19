d31a8e75f0635e3464e5103a7270ad3203c78d32
Emil-Sila <emil.siladi@gmail.com>
2015-12-19 18:45:15 +0100

    MeetingsTimeslots test fix

--- tests/Employees/Planners/MeetingTimeslotsTest.php
+++ tests/Employees/Planners/MeetingTimeslotsTest.php
@@ -100,14 +100,14 @@ class MeetingTimeslotsTest extends \TestCase
 
         $response = $this->actingAs($this->planner)
             ->json('GET', 'employees/planners/groups/'.$test_group->id.'/meetings/'.$this->meeting->id.'/timeslots'.$this->meeting_timeslot->id);
-        $response->seeStatusCode(404);
+        $response->seeStatusCode(403);
 
 
         $test_meeting = Meeting::where('id', '<>', $this->meeting->id)->firstOrFail();
 
         $response = $this->actingAs($this->planner)
             ->json('GET', 'employees/planners/groups/'.$this->group->id.'/meetings/'.$test_meeting->id.'/timeslots'.$this->meeting_timeslot->id);
-        $response->seeStatusCode(404);
+        $response->seeStatusCode(403);
     }
 
     public function testCreate()
@@ -154,13 +154,12 @@ class MeetingTimeslotsTest extends \TestCase
             'time_end' => '2015-12-17 15:00:00',
         ];
         $response = $this->actingAs($this->planner)
-            ->json('PUT', 'employees/planners/groups/'.$this->group->id.'/meetings/'.$this->meeting->id.'/timeslots/'.$this->meeting_timeslot->id,
-                $test_data);
+            ->json('PUT', 'employees/planners/groups/'.$this->group->id.'/meetings/'.$this->meeting->id.'/timeslots/'.$this->meeting_timeslot->id, $test_data);
         $response->assertResponseOk();
         $response->seeJson($test_data);
 
-        //$this->assertEquals($test_data['time_start'], $this->meeting_timeslot->time_start);
-        //$this->assertEquals($test_data['time_end'], $this->meeting_timeslot->time_end);
+        $this->assertEquals($test_data['time_start'], $this->meeting_timeslot->time_start);
+        $this->assertEquals($test_data['time_end'], $this->meeting_timeslot->time_end);
     }
 
     public function testEmployeeUpdate()
