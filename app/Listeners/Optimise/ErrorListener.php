a7c2a9c0a4ed62654f78320e288e5deeff2ccbba
Claudio Cardinale <cardinale.claudio@gmail.com>
2015-12-20 02:48:55 +0100

    minor fix

--- app/Listeners/Optimise/ErrorListener.php
+++ app/Listeners/Optimise/ErrorListener.php
@@ -29,15 +29,16 @@ class ErrorListener
         //
         \Log::info('problems during optimise (company id = '.$event->getCompany()->id.'): '.$event->getError());
         $event->getCompany()->fresh();
-        self::sendEmail($event->getCompany()->email);
+        self::sendEmail($event->getCompany()->email, $event->getError());
     }
 
     /**
      * @param string $email
+     * @param string $error
      */
-    static private function sendEmail($email)
+    static private function sendEmail($email, $error)
     {
-        \Mail::queue('emails.optimise.error', ['error' => $event->getError()], function ($message) use($email) {
+        \Mail::queue('emails.optimise.error', ['error' => $error], function ($message) use($email) {
             $message->from(config('mail.from.address'), config('mail.from.name'));
             $message->to($email)->subject('Problems during optimisation');
         });
