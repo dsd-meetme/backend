f3e17dcb79d4e06f1091df9fe238b6474fc81043
Emil-Sila <emil.siladi@gmail.com>
2015-12-21 18:51:32 +0100

    MeetingsTimeslots test fix

--- tests/Employees/Planners/MeetingTimeslotsTest.php
+++ tests/Employees/Planners/MeetingTimeslotsTest.php
@@ -26,7 +26,7 @@ class MeetingTimeslotsTest extends \TestCase
         $this->employee = $this->company->employees()->with('groups')->first();
         $this->group = $this->employee->groups->first();
         $this->planner = $this->group->planner;
-        $this->meeting = $this->group->meetings->first();
+        $this->meeting = $this->group->meetings->with('meeting')->first();
 
         $this->data= [
             'time_start' => '2015-12-17 12:00:00',
