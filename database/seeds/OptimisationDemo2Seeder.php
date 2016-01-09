ccf51f19b5d0ea16a4d97d3b8c8882af1f9786b7
Claudio Cardinale <cardinale.claudio@gmail.com>
2016-01-09 16:05:11 +0100

    demo seeder 2

--- database/seeds/OptimisationDemo2Seeder.php
+++ database/seeds/OptimisationDemo2Seeder.php
@@ -13,7 +13,7 @@ class OptimisationDemo2Seeder extends Seeder
     {
         //
         self::complexModel();
-
+        self::complexModel2();
     }
 
     static private function  complexModel()
@@ -66,5 +66,45 @@ class OptimisationDemo2Seeder extends Seeder
     static private function addTimeInterval(\DateTime $date, $multiplier=1)
     {
         return $date->add(new \DateInterval('PT' . config('app.timeslots.duration') * $multiplier . 'S'));
+    }
+
+    static private function complexModel2()
+    {
+        $company = factory(\plunner\Company::class)->create(['password' => bcrypt('test')]);
+        $employees = factory(\plunner\Employee::class, 3)->make(['password' => bcrypt('test')])->each(function ($employee) use ($company) {
+            $company->employees()->save($employee);
+            $employee->calendars()->save(factory(\plunner\Calendar::class)->make(['enabled' => true]));
+        });
+
+        $group1 = factory(\plunner\Group::class)->make();
+        $group2 = factory(\plunner\Group::class)->make();
+        $group3 = factory(\plunner\Group::class)->make();
+        $company->groups()->save($group1);
+        $company->groups()->save($group2);
+        $company->groups()->save($group3);
+        $group1->employees()->attach([$employees[0]->id, $employees[1]->id, $employees[2]->id]);
+        $group2->employees()->attach([$employees[1]->id, $employees[2]->id]);
+        $group3->employees()->attach([$employees[2]->id]);
+
+        $meeting1 = factory(\plunner\Meeting::class)->make(['duration' => 1 * config('app.timeslots.duration')]);
+        $group1->meetings()->save($meeting1);
+        $meeting2 = factory(\plunner\Meeting::class)->make(['duration' => 2 * config('app.timeslots.duration')]);
+        $group2->meetings()->save($meeting2);
+        $meeting3 = factory(\plunner\Meeting::class)->make(['duration' => 3 * config('app.timeslots.duration')]);
+        $group3->meetings()->save($meeting3);
+
+        $now = (new \DateTime())->modify('next monday');
+        $timeslots1 = ['time_start' => clone $now, 'time_end' => self::addTimeInterval(clone $now, 4)];
+        $meeting1->timeslots()->create($timeslots1);
+        $meeting3->timeslots()->create($timeslots1);
+        $meeting2->timeslots()->create($timeslots1);
+        $timeslotsE1 = ['time_start' => clone $now, 'time_end' => self::addTimeInterval(clone $now, 1)];
+        $timeslotsE2 = ['time_start' => self::addTimeInterval(clone $now, 3), 'time_end' => self::addTimeInterval(clone $now, 4)];
+        $employees[0]->calendars()->first()->timeslots()->create($timeslotsE1);
+        $employees[1]->calendars()->first()->timeslots()->create($timeslotsE2);
+        $employees[2]->calendars()->first()->timeslots()->create($timeslotsE2);
+
+        print_r($company->toArray());
+        print_r($employees->toArray());
     }
 }
